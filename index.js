0750851f8f11b0efee074d5d7b1045fd3ba623fe
Oscar Adams <oadams.co@gmail.com>
2018-07-20 20:45:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,6 +71,6 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if(cardNumber false){
-    return ``
+    return `Sorry, we don't have a credit card on file for you.`
   }
 }
