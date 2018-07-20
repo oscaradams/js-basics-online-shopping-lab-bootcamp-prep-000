e0b81f4063241cd86f3d47a5863b17e7d26504ac
Oscar Adams <oadams.co@gmail.com>
2018-07-20 20:55:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ function placeOrder(cardNumber) {
   if(cardNumber === undefined){
     return `Sorry, we don't have a credit card on file for you.`
   }else {
-    return `Your total cost is $${total()}, which will be charged to the card ${card}.`
+    purchase.push(`Your total cost is $${total()}, which will be charged to the card ${card}.`)
   }
   
 }
