ad7aecef109e37abe786f578a41cfe7dc97138c2
Oscar Adams <oadams.co@gmail.com>
2018-07-20 20:25:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,19 +10,56 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  
+  
+  var list = {};
+  var itemName = item;
+  var price = Math.floor(Math.random() *101);
+  
+  list['itemName'] = itemName;
+  list[`itemPrice`] = price;
+  
+  cart.push(list);
+  
+  return `${itemName} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  var i = cart.length;
+  var nameList = [];
+  while(i>0){
+    --i
+    nameList.unshift(` ${cart[i]['itemName']} at $${cart[i]['itemPrice']}`)
+    
+  }
+  if(cart.length === 0 ){
+    return 'Your shopping cart is empty.'
+  }
+  if(cart.length === 1){
+    return `In your cart, you have ${cart[0]['itemName']} at $${cart[0]['itemPrice']}.`
+  }
+  else{
+
+    return `In your cart, you have${nameList.slice(0, nameList.length - 1)}, and${nameList.pop()}.`
+  }
 }
 
 function total() {
-  // write your code here
+  var p = Number();
+  var t = cart.length;
+  do{--t
+    p += cart[t]['itemPrice']
+  }
+  while(0<t)
+  return p
+  
 }
 
+
+
 function removeFromCart(item) {
-  // write your code here
+
+  
 }
 
 function placeOrder(cardNumber) {
