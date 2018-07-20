c1a7eeb45c2746597d0d5b631f8436823725c534
Oscar Adams <oadams.co@gmail.com>
2018-07-20 20:49:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -73,7 +73,8 @@ function placeOrder(cardNumber) {
   if(cardNumber === undefined){
     return `Sorry, we don't have a credit card on file for you.`
   }
-  if(cardNumber === true)
+  if(cardNumber === true){
   return `FUCK`
+  }
   
 }
